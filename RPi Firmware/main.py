b1442045b4dad03287abef55baa1e4db9f0a5731
paramintrt94 <36721661+paramintrt94@users.noreply.github.com>
2021-09-28 15:55:16 -0400

    Initial Commit
    Initial commit

---
+++ RPi Firmware/main.py
@@ -0,0 +1,16 @@
+import RPI.GPIO as GPIO
+import time
+
+# Input Switch definitions
+switch1 = 2
+switch2 = 3
+switch3 = 4
+switch4 = 17
+switch5 = 27
+
+# Relay definitions
+led1_relay = 5
+led2_relay = 6
+led3_relay = 13
+led4_relay = 19
+led5_relay = 26
