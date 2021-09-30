776baf98adb2bac87c841357f23333ac9f16185e
Paramintr Tienpasertkij <ptienpasertkij@most.org>
2021-09-30 17:20:46 -0400

    Added button functionality

--- RPi Firmware/main.py
+++ RPi Firmware/main.py
@@ -1,19 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 from time import sleep
-from gpiozero import Button
-from gpiozero import LED
+from gpiozero import Button, LED
 import atexit
 
-button1 = Button(2)
-button2 = Button(3)
-
 # Input Switch definitions
-switch1 = 2
-switch2 = 3
-switch3 = 4
-switch4 = 17
-switch5 = 27
+switch1 = Button(2)
+switch2 = Button(3)
+switch3 = Button(4)
+switch4 = Button(17)
+switch5 = Button(27)
 
 # Relay definitions
 led1_relay = 5
@@ -31,6 +27,8 @@ led5 = LED(led5_relay)
 ledring = LED(ledring_relay)
 all_led = [led1, led2, led3, led4, led5]
 
+fake_timer = 30
+
 def turn_leds_off():
     for led in all_led:
         led.off()
@@ -42,12 +40,13 @@ def turn_leds_on():
         led.on()
         sleep(0.2)
     ledring.off()
+    
+def printHi():
+    print("Hi")
 
 if __name__ == "__main__":
-    turn_leds_off()
-    sleep(0.5)
+    # ~ turn_leds_off()
+    # ~ sleep(0.5)
+    # ~ turn_leds_on
     while True:
-        turn_leds_on()
-        atexit.register(turn_leds_off)
-    
-    atexit.register(turn_leds_off)
+        switch1.when_released = printHi
